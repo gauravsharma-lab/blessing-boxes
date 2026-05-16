e760562d869e96b49d1c93d8c534692560657291
Gaurav Sharma <gauravsharmash84@gmail.com>
2026-05-16 12:11:53 +0530

    Switch to SMTP port 465 (SSL) for Render compatibility

--- blessingboxes/settings.py
+++ blessingboxes/settings.py
@@ -136,8 +136,9 @@ WHITENOISE_MANIFEST_STRICT = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'blessingsboxes5@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'zspmuwnvkhyzqtok')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
